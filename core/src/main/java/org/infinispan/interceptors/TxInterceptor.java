5ea617609aa97d76aeccdd5f22fd565c31752f36
Pedro Ruivo <pruivo@gsd.inesc-id.pt>
2012-03-12 18:36:12 +0000

    * remote transaction are removed from the remote table in Total Order Manager. remove the condition in TxInterceptor

--- core/src/main/java/org/infinispan/interceptors/TxInterceptor.java
+++ core/src/main/java/org/infinispan/interceptors/TxInterceptor.java
@@ -118,9 +118,8 @@ public class TxInterceptor extends CommandInterceptor {
    @Override
    public Object visitCommitCommand(TxInvocationContext ctx, CommitCommand command) throws Throwable {
       if (this.statisticsEnabled) commits.incrementAndGet();
-      Object result = invokeNextInterceptor(ctx, command);
-      //In total order, we have remote transaction corresponding a local transaction
-      if (!ctx.isOriginLocal() || configuration.isTotalOrder()) {
+      Object result = invokeNextInterceptor(ctx, command);      
+      if (!ctx.isOriginLocal()) {
          txTable.remoteTransactionCommitted(ctx.getGlobalTransaction());
       }
       return result;
@@ -130,7 +129,7 @@ public class TxInterceptor extends CommandInterceptor {
    public Object visitRollbackCommand(TxInvocationContext ctx, RollbackCommand command) throws Throwable {
       if (this.statisticsEnabled) rollbacks.incrementAndGet();
       //In total order, we have remote transaction corresponding a local transaction
-      if (!ctx.isOriginLocal() || configuration.isTotalOrder()) {
+      if (!ctx.isOriginLocal()) {
          txTable.remoteTransactionRollback(command.getGlobalTransaction());
       }
       return invokeNextInterceptor(ctx, command);

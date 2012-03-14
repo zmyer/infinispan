0a53c1d91ac426398252cc0c894a792b0b9a1ee2
Pedro Ruivo <pruivo@gsd.inesc-id.pt>
2012-03-14 10:55:38 +0000

    * BUGFIX: commit in local context when the transaction had no modifications (implicit transaction with CACHE_LOCAL_MODE flag enabled)  * BUGFIX: construct a versioned prepare command when commit in 1PC  * MODIFIED: remote the send of the rollback command when in 1PC with Total Order

--- core/src/main/java/org/infinispan/transaction/TransactionCoordinator.java
+++ core/src/main/java/org/infinispan/transaction/TransactionCoordinator.java
@@ -173,12 +173,15 @@ public class TransactionCoordinator {
          validateNotMarkedForRollback(localTransaction);
 
          if (trace) log.trace("Doing an 1PC prepare call on the interceptor chain");
-         PrepareCommand command = commandsFactory.buildPrepareCommand(localTransaction.getGlobalTransaction(), localTransaction.getModifications(), true);
+         PrepareCommand command = commandCreator.createPrepareCommand(localTransaction.getGlobalTransaction(), localTransaction.getModifications());
+         command.setOnePhaseCommit(true);
 
          try {
             invoker.invoke(ctx, command);
          } catch (Throwable e) {
-            handleCommitFailure(e, localTransaction);
+            if (!isOnePhaseTotalOrder()) { //in total order and 1PC, the rollback command is not needed
+               handleCommitFailure(e, localTransaction);
+            }
          }
       } else {
          CommitCommand commitCommand = commandCreator.createCommitCommand(localTransaction.getGlobalTransaction());

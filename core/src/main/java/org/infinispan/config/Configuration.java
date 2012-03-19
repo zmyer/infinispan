437416e5308c4a8cb06fb1b42b84c2e387a79e02
Galder Zamarreño <galder@zamarreno.com>
2012-03-19 11:40:00 +0100

    ISPN-1923 Fix old versioning configuration override

--- core/src/main/java/org/infinispan/config/Configuration.java
+++ core/src/main/java/org/infinispan/config/Configuration.java
@@ -1535,6 +1535,7 @@ public class Configuration extends AbstractNamedCacheConfigurationBean {
       transaction.accept(v);
       unsafe.accept(v);
       indexing.accept(v);
+      versioning.accept(v);
    }
 
    /**

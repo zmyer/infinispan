e0940648bfac330dc92a51d9a3a3f4fb672bc3af
Pedro Ruivo <pruivo@gsd.inesc-id.pt>
2012-05-23 11:47:16 +0100

    * bug: null pointer exception thrown when no thread local stuff exists

--- core/src/main/java/org/infinispan/stats/TransactionsStatisticsRegistry.java
+++ core/src/main/java/org/infinispan/stats/TransactionsStatisticsRegistry.java
@@ -2,7 +2,6 @@ package org.infinispan.stats;
 
 import org.infinispan.configuration.cache.Configuration;
 import org.infinispan.context.impl.TxInvocationContext;
-import org.infinispan.stats.translations.ExposedStatistics;
 import org.infinispan.stats.translations.ExposedStatistics.IspnStats;
 import org.infinispan.stats.translations.ExposedStatistics.TransactionalClasses;
 import org.infinispan.transaction.xa.GlobalTransaction;
@@ -46,11 +45,20 @@ public final class TransactionsStatisticsRegistry {
 
    public static void addValue(IspnStats param, double value) {
       TransactionStatistics txs = thread.get();
+      if (txs == null) {
+         log.info("Trying to add value " + value + " to parameter " + param +
+                        " but no transaction is associated to the thread");
+         return;
+      }
       txs.addValue(param, value);
    }
 
    public static void incrementValue(IspnStats param) {
       TransactionStatistics txs = thread.get();
+      if (txs == null) {
+         log.info("Trying to increment to parameter " + param + " but no transaction is associated to the thread");
+         return;
+      }
       txs.addValue(param, 1D);
    }
 
@@ -76,16 +84,29 @@ public final class TransactionsStatisticsRegistry {
       //NB: If I want to give up using the InboundInvocationHandler, I can create the remote transaction
       //here, just overriding the handlePrepareCommand
       TransactionStatistics txs = thread.get();
+      if (txs == null) {
+         log.info("Trying to invoke onPrepareCommand() but no transaction is associated to the thread");
+         return;
+      }
       txs.onPrepareCommand();
    }
 
    public static void setTransactionOutcome(boolean commit) {
       TransactionStatistics txs = thread.get();
+      if (txs == null) {
+         log.info("Trying to set outcome to " + (commit ? "Commit" : "Rollback") +
+                        " but no transaction is associated to the thread");
+         return;
+      }
       txs.setCommit(commit);
    }
 
    public static void terminateTransaction() {
       TransactionStatistics txs = thread.get();
+      if (txs == null) {
+         log.info("Trying to invoke terminate() but no transaction is associated to the thread");
+         return;
+      }
       txs.terminateTransaction();
 
       NodeScopeStatisticCollector dest = transactionalClassesStatsMap.get(txs.getTransactionalClass());
@@ -104,12 +125,20 @@ public final class TransactionsStatisticsRegistry {
 
    public static void addTakenLock(Object lock) {
       TransactionStatistics txs = thread.get();
+      if (txs == null) {
+         log.info("Trying to add lock [" + lock + "] but no transaction is associated to the thread");
+         return;
+      }
       txs.addTakenLock(lock);
    }
 
 
    public static void setUpdateTransaction() {
       TransactionStatistics txs = thread.get();
+      if (txs == null) {
+         log.info("Trying to invoke setUpdateTransaction() but no transaction is associated to the thread");
+         return;
+      }
       txs.setUpdateTransaction();
    }
 

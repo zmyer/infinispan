fce1bddf73cf8ad597a641889f5d2692bc75d364
Pedro Ruivo <pruivo@gsd.inesc-id.pt>
2012-07-03 16:43:52 +0100

    * fix: memory leak (some transaction wasn't removed from transaction table)

--- core/src/main/java/org/infinispan/transaction/totalorder/SequentialTotalOrderManager.java
+++ core/src/main/java/org/infinispan/transaction/totalorder/SequentialTotalOrderManager.java
@@ -33,6 +33,9 @@ public class SequentialTotalOrderManager extends BaseTotalOrderManager {
          log.trace("Exception while processing the rest of the interceptor chain", t);
          result = t;
          exception = true;
+         //if an exception is throw, the TxInterceptor will not remove it from the TxTable and the rollback is not 
+         //sent (with TO)
+         transactionTable.remoteTransactionRollback(prepareCommand.getGlobalTransaction());
       } finally {
          logProcessingFinalStatus(prepareCommand, result, exception);
          updateLocalTransaction(result, exception, prepareCommand.getGlobalTransaction());
